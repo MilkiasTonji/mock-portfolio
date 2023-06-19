1156535e31f5d7214c8c99ab879dd3a508c30b95
MilkiasTonji <tongisttonji@gmail.com>
2023-06-19 14:23:11 +0300

    Added two buttons namely Resume and contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Head from 'next/head'
 
 import profilePic from '../../public/images/profile/developer-pic-1.png'
 import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
+
+import {LinkArrow} from '../components/Icons'
 
 export default function Home() {
   return (
@@ -18,8 +21,17 @@ export default function Home() {
             <div className='w-1/2'><Image src={profilePic} alt='milkiasTonjiPic' /></div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text={"Turning Vision Into Reality With Code And Design."} className='!text-6xl !text-left' />
-              <p>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+              <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
+                <div className='flex items-center self-start mt-2'>
+                  <Link href="/dummy.pdf" target='_blank'
+                  className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark'
+                  download={true}
+                  >Resume <LinkArrow className={"w-6 ml-1"} /> </Link>
+                  <Link href="mailto:mikytonji1362@gmail.com" target='_blank'
+                  className='flex items-center text-dark ml-3 p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-dark hover:text-light border border-solid border-transparent border-dark'
+                  >Contact Me</Link>
+                </div>
             </div>
           </div>
         </Layout>
