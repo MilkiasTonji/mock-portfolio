113b6f8e55107457e953cc84b9b2ba4ab9667554
MilkiasTonji <tongisttonji@gmail.com>
2023-06-19 12:17:24 +0300

    Added social links with svg. Created re-usable react svg component by SVGR playground

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
+import { TwitterIcon, LinkedInIcon, GithubIcon, PinterestIcon, DribbbleIcon } from './Icons'
+import { motion } from 'framer-motion'
 
-const CustomLink = ({href, title, className=""})=> {
+const CustomLink = ({ href, title, className = "" }) => {
     const router = useRouter();
-    return(
+    return (
         <Link href={href} className={`${className} relative group ${router.asPath === href ? 'font-bold' : ''}`}>
             {title}
             <span className={`
@@ -13,36 +15,55 @@ const CustomLink = ({href, title, className=""})=> {
                 -bottom-0.5 rounded-md group-hover:w-full transition-[width] ease-in duration-300
                 ${router.asPath === href ? 'w-full font-bold' : 'w-0'}
             `}>
-            &nbsp;</span>
+                &nbsp;</span>
         </Link>
     )
 }
 
 
 const Navbar = () => {
-  return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav className='flex gap-4'>
-            <CustomLink href="/" title={"Home"} className='' /> 
-            <CustomLink href="/about" title={"About"} className='' /> 
-            <CustomLink href="/projects" title={"Projects"} className='' />
-            <CustomLink href="/articles" title={"Articles"} className='' /> 
-        </nav>
-        {/* bring logo component here */}
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-            <Logo />
-        </div>
-        <nav>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
+    return (
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            <nav className='flex gap-4'>
+                <CustomLink href="/" title={"Home"} className='' />
+                <CustomLink href="/about" title={"About"} className='' />
+                <CustomLink href="/projects" title={"Projects"} className='' />
+                <CustomLink href="/articles" title={"Articles"} className='' />
+            </nav>
+            {/* bring logo component here */}
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo />
+            </div>
+            <nav className='flex items-center justify-center flex-wrap gap-4'>
+                <motion.a href="https://www.twitter.com/MilkiasTonji" target={'_blank'}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className='w-6'
+                ><TwitterIcon /></motion.a>
+                <motion.a href="https://www.linkedin.com/MilkiasTonji" target={'_blank'}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className='w-6'
+                ><LinkedInIcon /></motion.a>
+                <motion.a href="https://www.linkedin.com/MilkiasTonji" target={'_blank'}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className='w-6'
+                ><GithubIcon /></motion.a>
+                <motion.a href="https://www.linkedin.com/MilkiasTonji" target={'_blank'}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className='w-6'
+                ><PinterestIcon /></motion.a>
+                <motion.a href="https://www.linkedin.com/MilkiasTonji" target={'_blank'}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className='w-6'
+                ><DribbbleIcon /></motion.a>
 
-        </nav>
-    </header>
-  )
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
