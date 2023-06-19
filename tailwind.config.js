6d4be682138c06636307e75a839b8de50441c178
MilkiasTonji <tongisttonji@gmail.com>
2023-06-19 09:35:35 +0300

    Latest update on Jun19 2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+
+const {fontFamily} = require('tailwindcss/defaultTheme')
+
+
+
 module.exports = {
   content: [
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-mont)', ...fontFamily.sans],
+        mono: ['var(--font-mont)', ...fontFamily.mono] 
+      }
+    },
   },
   plugins: [],
 }
