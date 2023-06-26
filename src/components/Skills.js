aa4c2201a4ef9c3173b7f3c8097d90fb4094dc14
Milkias Tonji <tongisttonji@gmail.com>
2023-06-26 16:21:41 +0300

    minimal modifications on myskills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,22 +1,38 @@
 import React from 'react'
 
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const mySkills = [
-  "WEB",
-  "CSS",
-  "REACT.JS",
-  "NODE.JS"
+  { name: "CSS", x: "-5vw", y: "-10vw" },
+  { name: "REACT.JS", x: "5vw", y: "10vw" },
+  { name: "NODE.JS", x: "-10vw", y: "-15vw" },
+  { name: "Angular", x: "10vw", y: "15vw" },
+  { name: "JAVASCRIPT", x: "-15vw", y: "-20vw" },
+  { name: "HTML", x: "15vw", y: "20vw" },
+  { name: "CSS3", x: "-17vw", y: "0vw" },
+  { name: "GO", x: "16vw", y: "0vw" },
+  { name: "EXPRESS", x: "24vw", y: "5vw" },
+  { name: "MYSQL", x: "-24vw", y: "5vw" },
+  { name: "MONGODB", x: "-29vw", y: "0vw" },
+  { name: "REACT HOOKS", x: "29vw", y: "0vw" },
+  { name: "LARAVEL", x: "38vw", y: "-5vw" },
+  { name: "PHP", x: "-38vw", y: "-5vw" },
+  { name: "DOCKER", x: "-7vw", y: "10vw" },
+  { name: "RESTful API", x: "-12vw", y: "15vw" },
+  { name: "gRPC", x: "-19vw", y: "20vw" },
+  { name: "NEXT.js", x: "7vw", y: "-10vw" },
+  { name: "TYPESCRIPT", x: "12vw", y: "-15vw" },
+  { name: "TAILWIND CSS", x: "19vw", y: "-20vw" },
 ]
 
-const Skill = ({name, x, y}) => {
+const Skill = ({ name, x, y }) => {
   return (
     <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
-    py-3 px-6 shadow-dark cursor-pointer absolute ' 
-    whileHover={{scale: 1.05}}
-    initial={{x: 0, y: 0}}
-    animate={{x: x, y: y}}
-    transition={{duration: 1.5}}
+    py-3 px-6 shadow-dark cursor-pointer absolute '
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      animate={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
     >
       {name}
     </motion.div>
@@ -28,22 +44,22 @@ const Skill = ({name, x, y}) => {
 const Skills = () => {
   return (
     <>
-    <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
-    <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
-      
-    <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark'
-      whileHover={{scale: 1.05}}
-    >
-        WEB
-    </motion.div>
-    <Skill name={"CSS"} x="-5vw" y={"-10vw"} />
-      
-       {/* {
-        mySkills.map((sk, index)=> (
-          <Skill name={sk} x="-5vw" y={"-10vw"} key={index} />
-        ))
-       } */}
-    </div>
+      <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
+      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+
+        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark'
+          whileHover={{ scale: 1.05 }}
+        >
+          WEB
+        </motion.div>
+        {/* <Skill name={"CSS"} x="-5vw" y={"-10vw"} /> */}
+
+        {
+          mySkills.map((sk, index) => (
+            <Skill name={sk.name} x={sk.x} y={sk.y} key={index} />
+          ))
+        }
+      </div>
     </>
   )
 }
