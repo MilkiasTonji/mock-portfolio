f329e3a2d33d776a941ac4f162e7108b14b16a0b
Milkias Tonji <tongisttonji@gmail.com>
2023-06-29 01:07:36 +0300

    added education part and minimal other parts modifications

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,7 @@ import profilePic from '../../public/images/profile/developer-pic-2.jpg'
 import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
+import Education from '@/components/Education'
 
 //animate numbers up to the value passed from component
 const AnimatedNumbers = ({value}) => {
@@ -90,6 +91,7 @@ const about = () => {
                     </div>
                     <Skills />
                     <Experience />
+                    <Education />
                 </Layout>
             </main>
         </>
