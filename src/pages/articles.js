9547b30ac8474bfd0b2fdce0064b87c361000761
Milkias Tonji <tongisttonji@gmail.com>
2023-06-30 23:11:32 +0300

    Latest update on Jun30 2023

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -11,9 +11,13 @@ const articles = () => {
                 <meta name='description' content='any description' />
             </Head>
 
-            <main>
+            <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
                 <Layout className='pt-16'>
                     <AnimatedText text={"Words Can Change The World!"} className='mb-16 text-6xl'/>
+                    <ul className='grid grid-cols-12 gap-16'>
+                        <li>Featured articles-1</li>
+                        <li>Featured articles-2</li>
+                    </ul>
                 </Layout>
             </main>
         </>
