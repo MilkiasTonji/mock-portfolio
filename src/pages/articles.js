3539ed43018cf8eb305113e45d12af2450142b52
Milkias Tonji <tongisttonji@gmail.com>
2023-07-01 15:50:16 +0300

    Finishing up the featured articles component

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -12,21 +12,22 @@ import { motion } from 'framer-motion'
 // instead of image now use FramerImage component
 const FramerImage = motion(Image);
 
-const FeaturedArticle = ({img, title,time, summary,link }) => {
+const FeaturedArticle = ({ img, title, time, summary, link }) => {
     return (
-        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative'>
+            <div className={`absolute top-0 -right-3  bg-dark rounded-br-3xl -z-10 w-[101%] h-[103%] rounded-2xl`} />
             <Link href={link} target='_blank'
                 className={`'w-full inline-block cursor-pointer overflow-hidden rounded-lg'`}>
-                <FramerImage src={img} alt={title} className='w-full h-auto' 
+                <FramerImage src={img} alt={title} className='w-full h-auto'
                     whileHover={{ scale: 1.05 }}
-                    transition={{duration: 0.2}}
-                /> 
-             </Link>
-             <Link href={link} target='_blank'>
+                    transition={{ duration: 0.2 }}
+                />
+            </Link>
+            <Link href={link} target='_blank'>
                 <h2 className='capitalize text-2xl font-bold my-2 hover:underline'>{title}</h2>
-             </Link>
-             <p className='text-sm mb-2'>{summary}</p>
-             <span className='text-primary font-semibold'>{time}</span>
+            </Link>
+            <p className='text-sm mb-2'>{summary}</p>
+            <span className='text-primary font-semibold'>{time}</span>
         </li>
     )
 }
@@ -43,7 +44,7 @@ const articles = () => {
                 <Layout className='pt-16'>
                     <AnimatedText text={"Words Can Change The World!"} className='mb-16 text-6xl' />
                     <ul className='grid grid-cols-2 gap-16'>
-                        <FeaturedArticle 
+                        <FeaturedArticle
                             title={"Build A Custom Pagination Component In Reactjs From Scratch"}
                             summary={"Learn how to build a custom pagination component in ReactJS from scratch. \
                             Follow this step-by-step guide to integrate Pagination component in your ReactJS project."}
@@ -51,7 +52,7 @@ const articles = () => {
                             link={"/"}
                             img={article1}
                         />
-                        <FeaturedArticle 
+                        <FeaturedArticle
                             title={"Build A Custom Pagination Component In Reactjs From Scratch"}
                             summary={"Learn how to build a custom pagination component in ReactJS from scratch. \
                             Follow this step-by-step guide to integrate Pagination component in your ReactJS project."}
@@ -59,7 +60,7 @@ const articles = () => {
                             link={"/"}
                             img={article2}
                         />
-                       
+
                     </ul>
                 </Layout>
             </main>
