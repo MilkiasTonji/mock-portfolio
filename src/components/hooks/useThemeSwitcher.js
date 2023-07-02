bd75400879c232630eebe18b964200b7978a907b
Milkias Tonji <tongisttonji@gmail.com>
2023-07-02 21:12:51 +0300

    started working on theming the pages

---
+++ src/components/hooks/useThemeSwitcher.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const useThemeSwitcher = () => {
+    // css feature to detected the user's color preference
+    const preferDarkQuery = "(prefer-color-scheme: dark)";
+
+    
+
+  return (
+    <div>useThemeSwitcher</div>
+  )
+}
+
+export default useThemeSwitcher
