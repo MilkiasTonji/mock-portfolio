4ef8b9205e718266e969e8510118854c32a43240
Milkias Tonji <tongisttonji@gmail.com>
2023-07-06 23:45:58 +0300

    latest update on Jul6 2023 and fixed the them issues other minimal changes. The main functionality is almost done.

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -45,7 +45,9 @@ const useThemeSwitcher = () => {
         if(mode === "dark"){
             window.localStorage.setItem("theme", "dark");
             window.document.documentElement.classList.add("dark");
-        }else{
+        }
+        
+        if(mode === "light"){
             window.localStorage.setItem("theme", "light");
             window.document.documentElement.classList.remove("dark");
         }
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -19,7 +19,7 @@ const FramerImage = motion(Image);
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
     return (
-        <li className='col-span-1 w-full p-4 bg-light dark:bg-light border border-solid border-dark rounded-2xl relative'>
+        <li className='col-span-1 w-full p-4 bg-light dark:bg-dark border border-solid border-dark dark:border-light rounded-2xl relative'>
             <div className={`absolute top-0 -right-3  bg-dark dark:bg-light rounded-br-3xl -z-10 w-[101%] h-[103%] rounded-2xl`} />
             <Link href={link} target='_blank'
                 className={`'w-full inline-block cursor-pointer overflow-hidden rounded-lg'`}>
@@ -29,10 +29,10 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
                 />
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='capitalize text-2xl font-bold my-2 hover:underline mt-4'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 hover:underline mt-4 dark:text-light'>{title}</h2>
             </Link>
-            <p className='text-sm mb-2'>{summary}</p>
-            <span className='text-primary font-semibold'>{time}</span>
+            <p className='text-sm mb-2 dark:text-light'>{summary}</p>
+            <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
         </li>
     )
 }
@@ -79,13 +79,14 @@ const Article = ({ img, title, date, link }) => {
     initial={{y: 200}}
     whileInView={{y:0, transition: {duration: 0.5, ease: "easeInOut"}}}
     viewport={{once: true}}
-    className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'>
+    className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light dark:bg-dark text-dark dark:text-light 
+    first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light'>
         <MovingImg
             title={title}
             img={img}
             link={link}
         />
-        <span className='text-primary font-semibold pl-4'>{date}</span>
+        <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
     </motion.li>
     )
 }
