9cedb6ce41d327318c31c44dbf0c7ac4adcfcaae
Milkias Tonji <tongisttonji@gmail.com>
2023-07-10 09:16:51 +0300

    Adding hamburger icon styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,9 +29,9 @@ const Navbar = () => {
     return (
         <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'>
             <button className='flex flex-col justify-center items-center'>
-                <span className='bg-dark dark:bg-light block h-1 rounded-sm -translate-y-0.5'></span>
-                <span className='bg-dark dark:bg-light block h-1 rounded-sm'></span>
-                <span className='bg-dark dark:bg-light block h-1 rounded-sm translate-y-0.5'></span>
+                <span className='bg-dark dark:bg-light block h-0.5 w-6 rounded-sm -translate-y-0.5'></span>
+                <span className='bg-dark dark:bg-light block h-0.5 w-6 rounded-sm'></span>
+                <span className='bg-dark dark:bg-light block h-0.5 w-6 rounded-sm translate-y-0.5'></span>
             </button>
             <nav className='flex gap-4'>
                 <CustomLink href="/" title={"Home"} className='' />
