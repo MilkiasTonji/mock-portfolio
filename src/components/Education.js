916af15c3103d1f08a5f186b5d522d39c4beab67
Milkias Tonji <tongisttonji@gmail.com>
2023-07-22 16:57:58 +0300

    Making about page responsive has been completed

--- src/components/Education.js
+++ src/components/Education.js
@@ -35,16 +35,16 @@ const myEducations = [
 const Details = ({type, time, place, info}) => {
     const ref = useRef(null)
     
-    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center'>
+    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center md:w-[80%]'>
         <LiIcon reference={ref} />
         <motion.div
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration:0.5, type:"spring"}}
         >
-            <h3 className='capitalize font-medium text-2xl dark:text-light'>{type}</h3>
-            <span className='capitalize font-medium text-dark/75 dark:text-light/75'>{time} | {place}</span>
-            <p className='font-medium w-full dark:text-light'>{info}</p>
+            <h3 className='capitalize font-medium text-2xl dark:text-light sm:text-xl xs:text-lg'>{type}</h3>
+            <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>{time} | {place}</span>
+            <p className='font-medium w-full dark:text-light  md:text-sm'>{info}</p>
         </motion.div>
     </li>
 }
@@ -61,12 +61,13 @@ const Education = () => {
 
   return (
     <div className='my-40'>
-        <h2 className='font-bold text-6xl mb-32 w-full text-center dark:text-light'>Education</h2>
-        <div className='w-[75%] mx-auto relative'>
+        <h2 className='font-bold text-6xl mb-32 w-full text-center dark:text-light md:text-6xl xs:text-4xl md:mb-16'>Education</h2>
+        <div className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
             <motion.div
             style={{scaleY: scrollYProgress}}
-            ref={ref} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light' />
-            <ul className='w-full flex flex-col items-start justify-between ml-4'>
+            ref={ref} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+            md:w-[2px] md:left-[30px] xs:left-[20px]' />
+            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                 {
                     myEducations.map((edu, index)=> (
                         <Details 
