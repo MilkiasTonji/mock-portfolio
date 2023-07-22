3c2268db49d1d06195570e362123be6bb5da82c9
Milkias Tonji <tongisttonji@gmail.com>
2023-07-22 12:17:15 +0300

    Fixed minimal issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,20 +20,20 @@ export default function Home() {
       </Head>
       <main className='flex flex-col items-center text-dark w-full min-h-screen dark:text-light'>
         <Layout className='pt-0 md:pt-16 sm:pt-8'>
-          <div className='flex items-center justify-between w-full'>
-            <div className='w-1/2'>
+          <div className='flex items-center justify-between w-full lg:flex-col'>
+            <div className='w-1/2 md:w-full'>
               <Image src={profilePic} alt='milkiasTonjiPic'
                 priority={true}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
                 className='w-full h-auto lg:hidden md:inline-block md:w-full'
               />
               </div>
-            <div className='w-1/2 flex flex-col items-center self-center lg:w-full'>
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText text={"Turning Vision Into Reality With Code And Design."}
                className='!text-6xl !text-left xl:!text-5xl lg:!text-center md:!text-5xl sm:!text-3xl' />
               <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
-                <div className='flex items-center self-start mt-2'>
+                <div className='flex items-center self-start mt-2 lg:self-center'>
                   <Link href="/dummy.pdf" target='_blank'
                   className='flex items-center bg-dark text-light p-2.5 px-6 
                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
